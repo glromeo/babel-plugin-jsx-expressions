65aaa0d0a970c59807da2134169b8160563b8d8f
glromeo <glromeo@gmail.com>
2025-06-16 00:04:19 +0100

    deleted temp test file

--- test/input.jsx
+++
@@ -1,7 +0,0 @@
-
-        import { signal } from "@preact/signals";
-        function App() {
-          const props = signal({ class: 'a' });
-          return <div {...props.value} />;
-        }
-    
